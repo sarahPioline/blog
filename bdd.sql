1fefad0767fe7f9bb34ccfa92da941f44a5495de
user <user@grosbgdelanight.home>
2021-12-21 13:14:56 +0100

    entity

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 21, 2021 at 12:05 PM
+-- Generation Time: Dec 21, 2021 at 12:14 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -13,6 +13,49 @@ SET time_zone = "+00:00";
 --
 -- Database: `blog`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `AUTHORS`
+--
+
+CREATE TABLE `AUTHORS` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `COMMENT`
+--
+
+CREATE TABLE `COMMENT` (
+  `id` int(11) NOT NULL,
+  `Author` int(11) NOT NULL,
+  `Post` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Content` varchar(500) NOT NULL,
+  `Point` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `POST`
+--
+
+CREATE TABLE `POST` (
+  `Title` varchar(50) NOT NULL,
+  `Subtitle` varchar(50) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `Date` date NOT NULL,
+  `Images` varchar(50) NOT NULL,
+  `Author` int(11) NOT NULL,
+  `Tags` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -30,6 +73,31 @@ CREATE TABLE `Tags` (
 --
 
 --
+-- Indexes for table `AUTHORS`
+--
+ALTER TABLE `AUTHORS`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id` (`Id`);
+
+--
+-- Indexes for table `COMMENT`
+--
+ALTER TABLE `COMMENT`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`),
+  ADD KEY `Author` (`Author`),
+  ADD KEY `Post` (`Post`);
+
+--
+-- Indexes for table `POST`
+--
+ALTER TABLE `POST`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Title` (`Title`),
+  ADD KEY `Author` (`Author`),
+  ADD KEY `Tags` (`Tags`);
+
+--
 -- Indexes for table `Tags`
 --
 ALTER TABLE `Tags`
@@ -41,7 +109,43 @@ ALTER TABLE `Tags`
 --
 
 --
+-- AUTO_INCREMENT for table `AUTHORS`
+--
+ALTER TABLE `AUTHORS`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `COMMENT`
+--
+ALTER TABLE `COMMENT`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `POST`
+--
+ALTER TABLE `POST`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `Tags`
 --
 ALTER TABLE `Tags`
   MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `COMMENT`
+--
+ALTER TABLE `COMMENT`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `AUTHORS` (`Id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`Post`) REFERENCES `POST` (`Id`);
+
+--
+-- Constraints for table `POST`
+--
+ALTER TABLE `POST`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`Author`) REFERENCES `AUTHORS` (`Id`),
+  ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`Tags`) REFERENCES `Tags` (`Id`);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { TagModule } from './tag/tag.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [PostModule, CommentModule, TagModule, AuthorModule],
   controllers: [AppController],
   providers: [AppService],
 })
